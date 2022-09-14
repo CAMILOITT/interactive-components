d67540fe8fa067d1d21fe3ef9a638856ea9285a5
CAMILOITT <camilocamilo5@outlook.com>
2022-09-14 01:49:46 -0500

    add js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-const $form = document.getElementById('form');
+const $cardRank = document.getElementById('card-rank'),
+  $cardThank = document.getElementById('card-thank'),
+  $btnValue = document.querySelectorAll('.containerBtn__btn'),
+  $submit = document.getElementById('submit'),
+  $back = document.getElementById('back'),
+  $rank = document.getElementById('rank');
 
-$form.addEventListener('click', (e) => {
-  e.preventDefault();
+$submit.addEventListener('click', () => {
+  $cardRank.classList.add('hidden');
+  $cardThank.classList.remove('hidden');
+});
+
+$back.addEventListener('click', () => {
+  $cardThank.classList.add('hidden');
+  $cardRank.classList.remove('hidden');
+});
+
+$btnValue.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    rank.innerHTML = btn.innerHTML;
+  });
 });
